215655277d80f5e4291efa2feef0e4b7935d1e6c
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-10 19:30:15 -0400

    - add and structure projects for clean architecture

--- ProductService.Api/Program.cs
+++ ProductService.Api/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.VisualBasic.FileIO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(config =>
+config.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
 
