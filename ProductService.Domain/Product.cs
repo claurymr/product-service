5b6d7e51c57d1b386e66bdcaa507dada2c16d600
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-11 10:37:09 -0400

    change product category property to string and modify db table configurations.

--- ProductService.Domain/Product.cs
+++ ProductService.Domain/Product.cs
@@ -1,5 +1,3 @@
-using ProductService.Domain.Enums;
-
 namespace ProductService.Domain;
 
 public class Product
@@ -8,6 +6,6 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public double Price { get; set; }
-    public CategoryType Category { get; set; }
+    public string Category { get; set; } = string.Empty;
     public string Sku { get; set; } = string.Empty;
 }
