37180084a987ceb48ec85aff8b9ab44d1fa19134
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-11 10:39:12 -0400

    add action property of type actiontype to price history model and modify db table configurations.

--- ProductService.Domain/PriceHistory.cs
+++ ProductService.Domain/PriceHistory.cs
@@ -1,3 +1,5 @@
+using ProductService.Domain.Enums;
+
 namespace ProductService.Domain;
 
 public class PriceHistory
@@ -6,6 +8,7 @@ public class PriceHistory
     public Guid ProductId { get; set; }
     public double OldPrice { get; set; }
     public double NewPrice { get; set; }
+    public ActionType Action { get; set; }
     public DateTimeOffset Timestamp { get; set; }
     public Product Product { get; set; }
 }
