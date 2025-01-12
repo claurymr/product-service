d28316840769e6730553f3cf8ad99a5e76f14ef3
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-11 22:07:34 -0400

    add create product endpoint and map result

---
+++ ProductService.Api/Endpoints/Products/CreateProductEndpoint.cs
@@ -0,0 +1,31 @@
+using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using ProductService.Application.Mappings;
+using ProductService.Application.Products.CreateProducts;
+
+namespace ProductService.Api.Endpoints.Products;
+public class CreateProductEndpoint(IMediator mediator) : Endpoint<CreateProductCommand, Results<Created<Guid>, BadRequest>>
+{
+    private readonly IMediator _mediator = mediator;
+
+    public override void Configure()
+    {
+        Post("/products");
+        AllowAnonymous(); //For now
+    }
+
+    public override async Task<Results<Created<Guid>, BadRequest>> ExecuteAsync(CreateProductCommand req, CancellationToken ct)
+    {
+        var result = await _mediator.Send(req, ct);
+        var response = result.Match<IResult>(
+            guid => TypedResults.Created($"/products/{guid}", guid),
+            failed => TypedResults.BadRequest(failed.MapToResponse()));
+        return response switch
+        {
+            Created<Guid> success => success,
+            BadRequest badRequest => badRequest,
+            _ => throw new Exception()
+        };
+    }
+}
