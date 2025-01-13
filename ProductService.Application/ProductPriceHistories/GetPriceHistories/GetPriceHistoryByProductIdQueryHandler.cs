77f1f87c88de696f9cd2664a46e46b00164168d6
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-13 13:13:28 -0400

    add tests for price history handlers

---
+++ ProductService.Application/ProductPriceHistories/GetPriceHistories/GetPriceHistoryByProductIdQueryHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using ProductService.Application.Contracts;
+using ProductService.Application.Repositories;
+
+namespace ProductService.Application.ProductPriceHistories.GetPriceHistories;
+public class GetPriceHistoryByProductIdQueryHandler(IPriceHistoryRepository priceHistoryRepository)
+    : IRequestHandler<GetPriceHistoryByProductIdQuery, IEnumerable<PriceHistoryResponse>>
+{
+    // Declare repository private field
+    // Declare httpclient private field
+    // Call main ctor and initialize repository and httpclient
+    public Task<IEnumerable<PriceHistoryResponse>> Handle(GetPriceHistoryByProductIdQuery request, CancellationToken cancellationToken)
+    {
+        // get currency if present, from external api using httpclient
+        // get products with applied calculation of price, and mapped to currency
+        // return list of products
+        throw new NotImplementedException();
+    }
+}
