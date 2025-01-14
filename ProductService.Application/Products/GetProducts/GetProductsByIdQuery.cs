3a65c27c03cb1994fa3a625513ea12725ea410f7
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-14 15:37:56 -0400

    remove unnecessary files

--- ProductService.Application/Products/GetProducts/GetProductsByIdQuery.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using ProductService.Application.Contracts;
-
-namespace ProductService.Application.Products.GetProducts;
-public record GetProductsByIdQuery(
-    Guid Id,
-    string? Currency = null)
-    : IRequest<IEnumerable<ProductResponse>>;
